593843b08b89488245cf2eb93692a2cc4d1ead39
Charlie Jackson <charlie@charliejackson.com>
2020-07-15 20:22:52 +0100

    Updates

---
+++ precache-manifest.d74cebc271e27d89f76261971c60c382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33823fabb3b8edd5af08ba925dbdba2",
+    "url": "/personal-metrics/index.html"
+  },
+  {
+    "revision": "64b614cac76f43e27cf5",
+    "url": "/personal-metrics/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7004a634def331be33c",
+    "url": "/personal-metrics/static/js/2.3ab8da6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-metrics/static/js/2.3ab8da6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b614cac76f43e27cf5",
+    "url": "/personal-metrics/static/js/main.91f53a66.chunk.js"
+  },
+  {
+    "revision": "11bb02a87d0dce3a28c6",
+    "url": "/personal-metrics/static/js/runtime-main.b81ad20e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-metrics/static/media/logo.5d5d9eef.svg"
+  }
+]);
